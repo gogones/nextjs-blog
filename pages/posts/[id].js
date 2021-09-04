228bc14991d96c0918a2e9a27f13fb30d0f6acd2
Annas <aag.annas@gmail.com>
2021-09-05 01:44:31 +0700

    progress

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Date from '../../components/date'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData: {title, id, date, contentHtml} }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <article>
+        <h1 className={utilStyles.headingXl}>{title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
